7115119e60a977ada30ea24f2e55b27fc30be581
jiwondh <win6ter@naver.com>
2017-11-16 09:11:18 +0900

    viewController추가하고 outlet 생성

---
+++ myFirstiOSApp/myFirstiOSApp/ProductViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProductViewController.swift
+//  myFirstiOSApp
+//
+//  Created by oh jiwon on 2017. 11. 16..
+//  Copyright © 2017년 oh jiwon. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    @IBOutlet weak var ProductNameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
