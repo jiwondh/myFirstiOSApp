65309b030a8262385ebef2c73fec1548a4bb319f
jiwondh <win6ter@naver.com>
2017-11-16 14:03:26 +0900

    ContactViewController 생성

---
+++ myFirstiOSApp/myFirstiOSApp/ContactViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactViewController.swift
+//  myFirstiOSApp
+//
+//  Created by oh jiwon on 2017. 11. 16..
+//  Copyright © 2017년 oh jiwon. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
